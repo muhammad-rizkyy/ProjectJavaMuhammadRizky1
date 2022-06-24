46f8ac62705a31c7e73b6dd372956175532a0d21
Muhammad Rizky <m.rmuhammadrizky29@gmail.com>
2022-06-24 22:40:27 +0700

    tutup,batal,hapus

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
 
+import frame.KabupatenViewFrame;
 import helpers.koneksi;
 
 public class Main {
     public static void main(String[] args) {
-        koneksi.getConnection();
+//        koneksi.getConnection();
+        KabupatenViewFrame viewFrame = new KabupatenViewFrame();
+        viewFrame.setVisible(true);
     }
 }
--- src/main/java/frame/KabupatenInputFrame.java
+++ src/main/java/frame/KabupatenInputFrame.java
@@ -1,2 +1,28 @@
-package frame;public class KabupatenInputFrame {
+package frame;
+
+import javax.swing.*;
+
+public class KabupatenInputFrame extends JFrame{
+    private JPanel panel1;
+    private JPanel mainPanel;
+    private JTextField idTextField;
+    private JTextField namaTextField;
+    private JPanel buttonPanel;
+    private JButton simpanButton;
+    private JButton batalButton;
+
+    public KabupatenInputFrame(){
+        batalButton.addActionListener(e -> {
+            dispose();
+        });
+        init();
+    }
+
+    public void init(){
+        setContentPane(mainPanel);
+        setTitle("Input Kabupeten");
+        pack();
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
 }
